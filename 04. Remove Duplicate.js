8ea811af09b95da4d5a915e54d1b6fe448932011
sazzad_saju <saju.cse.hstu@gmail.com>
2024-10-29 12:50:55 +0600

    Add: optimal solution for rmvdupli

--- 04. Remove Duplicate.js
+++ 04. Remove Duplicate.js
@@ -1,4 +1,4 @@
-function RmvDuplicate(word){
+function rmvDupli(word){
     let temp = "";
     for(let char of word){
         if(temp.indexOf(char) == -1){
@@ -8,5 +8,18 @@ function RmvDuplicate(word){
     return temp;
 }
 
+//more optimal
+// function rmvDupli(str){
+//     let unique = new Set();
+//     let result = "";
+//     for(char of str){
+//         if(!unique.has(char)){
+//             unique.add(char)
+//             result += char;
+//         }
+//     }
+//     return result;
+// }
+
 let word = 'abccddeffaaab'
-console.log(RmvDuplicate(word))
+console.log(rmvDupli(word))
